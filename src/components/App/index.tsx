22712e30eede6184d2f99c169ddc32c28d682c29
starwort <tcphone93@gmail.com>
2022-04-16 13:30:10 +0100

    Add game over modal, load state from localStorage

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Board, IconButton, SettingsModal} from "..";
 import {ALL_WORDS_SET} from "../../consts/allWords";
-import {AnimationSpeed, FontSize, GameState, getNextWord, KeyboardLayout, sampleStarterWords, Theme} from "../../util";
+import {AnimationSpeed, FontSize, GameState, getNextWord, KeyboardLayout, loadGameOrGenerate, loadSettingsOrDefault, Theme, validateLocalStorage} from "../../util";
 import {Modal} from "../Modal";
 import aboutIconBlack from "./about_icon_black.svg";
 import aboutIconWhite from "./about_icon_white.svg";
@@ -10,26 +10,24 @@ import settingsIconBlack from "./settings_icon_black.svg";
 import settingsIconWhite from "./settings_icon_white.svg";
 
 export function App() {
-    const [guessedWords, setGuessedWords] = React.useState<string[]>([]);
+    validateLocalStorage();
+    const loadedGameState = loadGameOrGenerate();
+    const loadedSettings = loadSettingsOrDefault();
+    const [guessedWords, setGuessedWords] = React.useState(loadedGameState.guessedWords);
     const [currentWord, setCurrentWord] = React.useState("");
-    const [secretWords, setSecretWords] = React.useState(sampleStarterWords());
-    const [cleared, setCleared] = React.useState(0);
-    const [theme, setTheme] = React.useState<Theme>(Theme.DARK);
-    const [isGameOver, setIsGameOver] = React.useState(false);
+    const [secretWords, setSecretWords] = React.useState(loadedGameState.secretWords);
+    const [theme, setTheme] = React.useState<Theme>(loadedSettings.theme);
+    const [fontSize, setFontSize] = React.useState(loadedSettings.fontSize);
+    const [animationSpeed, setAnimationSpeed] = React.useState(loadedSettings.animationSpeed);
+    const [keyboardLayout, setKeyboardLayout] = React.useState(loadedSettings.keyboardLayout);
     const [settingsOpen, setSettingsOpen] = React.useState(false);
     const [aboutOpen, setAboutOpen] = React.useState(false);
-    const [fontSize, setFontSize] = React.useState(FontSize.MEDIUM);
-    const [animationSpeed, setAnimationSpeed] = React.useState(AnimationSpeed.MEDIUM);
-    const [
-        keyboardLayout, setKeyboardLayout
-    ] = React.useState<KeyboardLayout>(KeyboardLayout.QWERTY);
+    const [gameOverModalOpen, setGameOverModalOpen] = React.useState(false);
+    const [isGameOver, setIsGameOver] = React.useState(false);
 
     const gameState = React.useRef<GameState & {currentWord: string;}>({
         currentWord,
-        guessedWords,
-        secretWords,
-        foundSecretWords: new Map<string, number>(),
-        boards: new Map<string, number>(secretWords.map(secretWord => [secretWord, 0])),
+        ...loadedGameState,
     });
     gameState.current.currentWord = currentWord;
     gameState.current.guessedWords = guessedWords;
@@ -49,7 +47,6 @@ export function App() {
                         const nextWord = getNextWord([...guessedWords, ...secretWords, ...foundSecretWords.keys()]);
                         foundSecretWords.set(currentWord, guessedWords.length + 1);
                         gameState.current.boards.set(nextWord, guessedWords.length + 1);
-                        setCleared(cleared => cleared + 1);
                         setSecretWords([...secretWords, nextWord]);
                     }
                     setGuessedWords(words => [...words, currentWord]);
@@ -65,11 +62,13 @@ export function App() {
                 window.removeEventListener("keydown", listener);
             };
         }
-    }, [gameState, setSecretWords, setCleared, setGuessedWords, setCurrentWord, isGameOver]);
+    }, [gameState, setSecretWords, setGuessedWords, setCurrentWord, isGameOver]);
 
     const wordIsValid = currentWord.length !== 5 || ALL_WORDS_SET.has(currentWord);
 
-    const endGame = React.useCallback(() => setIsGameOver(true), [setIsGameOver]);
+    const endGame = React.useCallback(() => setGameOverModalOpen(true), [setGameOverModalOpen]);
+
+    const cleared = gameState.current.foundSecretWords.size;
 
     return <div className={`
         app
@@ -146,6 +145,23 @@ export function App() {
                 <li><a href="https://zaratustra.itch.io/dordle">Dordle</a> by Guilherme S. Töws</li>
                 <li><a href="https://www.nytimes.com/games/wordle/index.html">Wordle</a> by Josh Wardle</li>
             </ul>
+        </Modal>
+        <Modal
+            open={gameOverModalOpen}
+            onClose={() => {setGameOverModalOpen(false); setIsGameOver(true);}}
+            theme={theme}
+            title="Game Over"
+        >
+            {cleared > 100 && <>Well done!<br /></>}
+            You were able to clear {cleared} boards!
+            <hr />
+            <button onClick={() => {
+                navigator.clipboard.writeText(
+                    `Infinitudle - ${cleared} boards cleared:\n\n` +
+                    [...gameState.current.foundSecretWords.keys()].join("\n") +
+                    "\n\n" + window.location.href
+                );
+            }}>Copy results</button>
         </Modal>
     </div>;
 }
